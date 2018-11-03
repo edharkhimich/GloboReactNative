aed65ad42c85b9b59fab1ea043f68adda3b9ef55
Edgar Khimich <edgarkhimich@gmail.com>
2018-11-02 21:54:09 -0600

    add question component

---
+++ app/data/QuizQuestions.js
@@ -0,0 +1,41 @@
+export const QuizData ={
+    questions:[
+        {
+            question: 'What language do you use with libraries such as React and Angular ?',
+            answer1: 'PHP',
+            answer2: 'JAVA',
+            answer3: 'JavaScript',
+            answer4: 'Python',
+            correctAnswer: 'answer3',
+            key: '1A'
+        },
+
+        {
+            question: 'Which one of these is a way to store multiple values in a single variable ?',
+            answer1: 'string',
+            answer2: 'array',
+            answer3: 'number',
+            number4: 'boolean',
+            correctAnswer: 'answer2'
+            key: '1B'
+        },
+        {
+            question: 'How do you indicate values are not equal in JavaScript',
+            answer1: '===',
+            answer2: '=>',
+            answer3: '!==',
+            answer4: '<=',
+            correctAnswer: 'answer3',
+            key: '1C'
+        },
+        {
+            question: 'What markup language do you use to build a web page?',
+            answer1: 'HTML',
+            answer2: 'PYTHON',
+            answer3: 'C',
+            answer4: 'JAVA',
+            correctAnswer: 'answer1',
+            key: '1D'
+        }
+    ]
+}
